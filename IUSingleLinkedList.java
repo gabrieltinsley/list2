fbae4c379b57c1aeab7c3662e6c700f58ce8b185
Gabriel Tinsley <gabrieltinsley@u.boisestate.edu>
2024-10-22 14:03:11 -0600

    started singleLinkedList

---
+++ IUSingleLinkedList.java
@@ -0,0 +1,232 @@
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Single-linked node implementation of IndexedUnsortedList.
+ * An Iterator with working remove() method is implemented, but
+ * ListIterator is unsupported.
+ * 
+ * @author 
+ * 
+ * @param <T> type to store
+ */
+public class IUSingleLinkedList<T> implements IndexedUnsortedList<T> {
+	private Node<T> head, tail;
+	private int size;
+	private int modCount;
+	
+	/** Creates an empty list */
+	public IUSingleLinkedList() {
+		head = tail = null;
+		size = 0;
+		modCount = 0;
+	}
+
+	@Override
+	public void addToFront(T element) {
+		// TODO 
+		
+	}
+
+	@Override
+	public void addToRear(T element) {
+		// TODO 
+		
+	}
+
+	@Override
+	public void add(T element) {
+		// TODO 
+		
+	}
+
+	@Override
+	public void addAfter(T element, T target) {
+		// TODO 
+		
+	}
+
+	@Override
+	public void add(int index, T element) {
+		// TODO 
+		
+	}
+
+	@Override
+	public T removeFirst() {
+		// TODO 
+		return null;
+	}
+
+	@Override
+	public T removeLast() {
+		// TODO 
+		return null;
+	}
+
+	@Override
+	public T remove(T element) {
+		if (isEmpty()) {
+			throw new NoSuchElementException();
+		}
+		
+		boolean found = false;
+		Node<T> previous = null;
+		Node<T> current = head;
+		
+		while (current != null && !found) {
+			if (element.equals(current.getElement())) {
+				found = true;
+			} else {
+				previous = current;
+				current = current.getNext();
+			}
+		}
+		
+		if (!found) {
+			throw new NoSuchElementException();
+		}
+		
+		if (size() == 1) { //only node
+			head = tail = null;
+		} else if (current == head) { //first node
+			head = current.getNext();
+		} else if (current == tail) { //last node
+			tail = previous;
+			tail.setNext(null);
+		} else { //somewhere in the middle
+			previous.setNext(current.getNext());
+		}
+		
+		size--;
+		modCount++;
+		
+		return current.getElement();
+	}
+
+	@Override
+	public T remove(int index) {
+		// TODO 
+		return null;
+	}
+
+	@Override
+	public void set(int index, T element) {
+		// TODO 
+		
+	}
+
+	@Override
+	public T get(int index) {
+		// TODO 
+		return null;
+	}
+
+	@Override
+	public int indexOf(T element) {
+		Node<T> current = head;
+        int currentIndex = 0;
+
+        while (current != null && !current.getElement().equals(element)) {
+            current = current.getNext();
+            currentIndex++;
+        }
+
+        if(current == null) {
+            currentIndex = -1;
+        }
+
+		return currentIndex;
+	}
+
+	@Override
+	public T first() {
+		if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+		return head.getElement();
+	}
+
+	@Override
+	public T last() {
+		if (isEmpty()) {
+            throw new NoSuchElementException();
+        }
+		return tail.getElement();
+	}
+
+	@Override
+	public boolean contains(T target) {
+		return (indexOf(target) != -1);
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return size == 0;
+	}
+
+	@Override
+	public int size() {
+		return size;
+	}
+
+    @Override
+	public String toString() {
+		StringBuilder str = new StringBuilder();
+		str.append("[");
+		for (T element : this) {
+			str.append(element.toString());
+			str.append(", ");
+		}
+		if (size > 0) {
+			str.delete(str.length() - 2, str.length()); // remove trailing ", "
+		}
+		str.append("]");
+		return str.toString();
+	}
+
+    @Override
+	public Iterator<T> iterator() {
+		return new SLLIterator();
+	}
+
+	@Override
+	public ListIterator<T> listIterator() {
+		throw new UnsupportedOperationException();
+	}
+
+	@Override
+	public ListIterator<T> listIterator(int startingIndex) {
+		throw new UnsupportedOperationException();
+	}
+
+	/** Iterator for IUSingleLinkedList */
+	private class SLLIterator implements Iterator<T> {
+		private Node<T> nextNode;
+		private int iterModCount;
+		
+		/** Creates a new iterator for the list */
+		public SLLIterator() {
+			nextNode = head;
+			iterModCount = modCount;
+		}
+
+		@Override
+		public boolean hasNext() {
+			// TODO 
+			return false;
+		}
+
+		@Override
+		public T next() {
+			// TODO 
+			return null;
+		}
+		
+		@Override
+		public void remove() {
+			// TODO
+		}
+	}
+}
